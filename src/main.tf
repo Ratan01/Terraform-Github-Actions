6e747d687bad81c1c9c29f8126664232dc778ec8
Ratan01 <ratanrudra010@gmail.com>
2023-07-18 20:23:47 +0530

    main.tf update

--- src/main.tf
+++ src/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 # windows virtual machine information
-resource "azurerm_windows_virtual_machine" "virtualmachine" {
+resource "azurerm_windows_virtual_machine" "vm12" {
   name                = "tf-vm"
   resource_group_name = azurerm_resource_group.RG.name
   location            = azurerm_resource_group.RG.location
